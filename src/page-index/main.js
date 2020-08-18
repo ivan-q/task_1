3874f5e2b3d708ff4aaec0c2d97822c9e85f6f39
Ivan Vlasenko <5834791@mail.ru>
2020-08-18 21:39:57 +0300

    Refactoring is almost ready. Moved components in their own folders

--- src/page-index/main.js
+++ src/page-index/main.js
@@ -5,15 +5,15 @@ require('../scss/_var_mix.scss')
 require('../css/main.css')
 require('./page.css')
 require('../scss/main.scss')
-require('../scss/nav.scss') //Navigation bar
-require('../scss/header.scss')
-require('../scss/slider_section.scss')
-require('../scss/color_change.scss')
-require('../scss/bg_section.scss')
-require('../scss/simple_section.scss')
-require('../scss/product_list.scss')
-require('../scss/signup.scss')
-require('../scss/footer.scss') //Footer
+require('../partials/nav/nav.scss') //Navigation bar
+require('../partials/header/header.scss')
+require('../partials/slider_section/slider_section.scss')
+require('../partials/color_change/color_change.scss')
+require('../partials/bg_section/bg_section.scss')
+require('../partials/simple_section/simple_section.scss')
+require('../partials/product_list/product_list.scss')
+require('../partials/signup/signup.scss')
+require('../partials/footer/footer.scss') //Footer
 require('../scss/pixel_perfect_1440px.scss')
 require('../scss/pixel_perfect_375px.scss')
 
