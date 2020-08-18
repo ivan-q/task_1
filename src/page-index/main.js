c5ecd68e8872111e7e0e9eddddfa39dd4d5f5e3c
Ivan Vlasenko <5834791@mail.ru>
2020-08-18 20:15:49 +0300

    Refactoring of styles is almost ready

--- src/page-index/main.js
+++ src/page-index/main.js
@@ -1,17 +1,19 @@
 import { Bar } from '../js/bar.es6'
 const { Foo } = require('../js/foo.cjs')
-// require('bootstrap/dist/css/bootstrap.css')
 require('normalize.css/normalize.css')
 require('../scss/_var_mix.scss')
 require('../css/main.css')
 require('./page.css')
 require('../scss/main.scss')
-require('../scss/signup.scss')
-require('../scss/nav.scss')
+require('../scss/nav.scss') //Navigation bar
+require('../scss/header.scss')
+require('../scss/slider_section.scss')
 require('../scss/color_change.scss')
-require('../scss/slider.scss')
-require('../scss/footer.scss')
-require('../scss/tablet.scss')
+require('../scss/bg_section.scss')
+require('../scss/simple_section.scss')
+require('../scss/product_list.scss')
+require('../scss/signup.scss')
+require('../scss/footer.scss') //Footer
 require('../scss/pixel_perfect_1440px.scss')
 require('../scss/pixel_perfect_375px.scss')
 
